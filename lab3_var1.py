027481fc2533f033a632d75dc8092f7f9f7537e9
Vova <vova-lebedev-97@mail.ru>
2023-09-03 14:57:48 +0300

    test2

---
+++ lab3_var1.py
@@ -0,0 +1,36 @@
+
+
+
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class TestSearchInput(unittest.TestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument("window-size=1920,1080")
+        self.driver = webdriver.Chrome(options)
+    
+    def test_search(self):
+        driver = self.driver
+        driver.get("http://www.google.com/")
+        driver.find_element(By.NAME, "q").send_keys("Hello" + Keys.ENTER)
+        time.sleep(5)
+        driver.find_element(By.NAME, "q").send_keys("World" + Keys.ENTER)
+        time.sleep(5)
+        value = driver.find_element(By.NAME, "q")
+        search_result = value.get_attribute("value")
+        self.assertEqual(search_result, "HelloWorld")
+        time.sleep(5)
+        
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
